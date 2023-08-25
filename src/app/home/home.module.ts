f24353b03c4fa7aab4fc5c8f46d00adef2694c42
aljofre <a.jofre@gmail.com>
2023-08-25 01:00:24 -0400

    hhhh

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,13 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { HomePageRoutingModule } from './home-routing.module';
 import { HomePage } from './home.page';
+import { MatDatepickerModule, MatNativeDateModule } from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,  // Asegúrate de que esto está importado y declarado aquí
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   declarations: [HomePage]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,45 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+  animations: [
+    trigger('titleAnimation', [
+      state('center', style({
+        transform: 'translateX(0)',
+        opacity: 1
+      })),
+      state('right', style({
+        transform: 'translateX(100%)',
+        opacity: 0.2
+      })),
+      state('left', style({
+        transform: 'translateX(-100%)',
+        opacity: 0.2
+      })),
+      transition('* => right', animate('2500ms')),
+      transition('* => left', animate('2500ms')),
+      transition('left => center', animate('2500ms'))
+    ])
+  ]
 })
+
 export class HomePage {
   user: { usuario: string, password: string };
+  titleState = 'center';
+  animateTitle() {
+    if (this.titleState === 'center') {
+      this.titleState = 'right';
+    } else if (this.titleState === 'right') {
+      this.titleState = 'left';
+    } else {
+      this.titleState = 'center';
+    }
+  }
+
 
   constructor(private router: Router) {
     this.user = { usuario: '', password: '' };
