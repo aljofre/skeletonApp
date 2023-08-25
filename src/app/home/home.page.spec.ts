0777f3e736418599b09850302f8f9b4cec8abb20
aljofre <a.jofre@gmail.com>
2023-08-25 18:14:04 -0400

    comeeetsss

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -1,26 +1,29 @@
+// Estás importando las dependencias necesarias.
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
 import { HomePage } from './home.page';
 
-
-
+// Comienza la descripción del grupo de pruebas para el componente HomePage.
 describe('HomePage', () => {
   let component: HomePage;
   let fixture: ComponentFixture<HomePage>;
 
+  // Antes de cada prueba, configura el entorno de prueba para el componente.
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [HomePage],
-      imports: [IonicModule.forRoot()]
+      declarations: [HomePage], // Declaras el componente que estás probando.
+      imports: [IonicModule.forRoot()] // Importas las dependencias necesarias.
     }).compileComponents();
 
+    // Creas una instancia del componente y detectas los cambios iniciales.
     fixture = TestBed.createComponent(HomePage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
+  // Una prueba básica que verifica si el componente se crea correctamente.
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
+
--- src/app/myservicio.service.ts
+++ src/app/myservicio.service.ts
@@ -3,7 +3,18 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class MyservicioService {
+export class ThemeService {
+
+  private darkMode = false;
 
   constructor() { }
+
+  toggleTheme() {
+    this.darkMode = !this.darkMode;
+    document.body.classList.toggle('dark', this.darkMode);
+  }
+
+  isDarkMode() {
+    return this.darkMode;
+  }
 }
