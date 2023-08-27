3e8ded3057636826f0891fc4c2408d75aeaa3a40
Alvaro Jofre <a.jofrema@gmail.com>
2023-08-27 03:48:22 -0400

    dele

--- src/app/access/access.page.ts
+++ src/app/access/access.page.ts
@@ -1,56 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { ActionSheetController } from '@ionic/angular';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-access',
-  templateUrl: './access.page.html',
-  styleUrls: ['./access.page.scss'],
+  templateUrl: 'access.page.html',
+  styleUrls: ['access.page.scss']
 })
-export class AccessPage implements OnInit {
 
-  titleState = 'center';
-  nombreState = 'normal';
-  apellidoState = 'normal';
-
-  animateTitle() {
-    if (this.titleState === 'center') {
-      this.titleState = 'right';
-    } else if (this.titleState === 'right') {
-      this.titleState = 'left';
-    } else {
-      this.titleState = 'center';
-    }
-  }
-
-  constructor(public actionSheetController: ActionSheetController) { }
-
-
-  ngOnInit() {
-  }
-
-  async presentActionSheet() {
-    const actionSheet = await this.actionSheetController.create({
-      header: 'elije modelo de GPS',
-      buttons: [{
-        text: 'GV55',
-        handler: () => {
-          console.log('gv55 comandos..');
-        }
-      }, {
-        text: 'GV75',
-        handler: () => {
-          console.log('gv75 comandos..');
-        }
-      }, {
-        text: 'GV300',
-        handler: () => {
-          console.log('gv300 comandos..');
-        }
-      }]
-    });
-    await actionSheet.present();
-  }
-  
-
-  }
-
+export class AccessPage {
+    // Código para tu página de acceso, si necesitas algo específico.
+}
