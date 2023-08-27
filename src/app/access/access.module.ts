fd56e966c474abecd919e69e0ccaefd57cd490f5
Alvaro Jofre <a.jofrema@gmail.com>
2023-08-27 05:01:25 -0400

    aa

--- src/app/access/access.module.ts
+++ src/app/access/access.module.ts
@@ -3,11 +3,10 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { AccessPageRoutingModule } from './access-routing.module';
-import { AccessPage } from './access.page';
+import { AccessPage } from './access.page'; // Corrección aquí
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
-
 
 @NgModule({
   imports: [
