bf03de8959a9b47ae05a37e7d5b95a5ba42037bc
Alvaro Jofre <a.jofrema@gmail.com>
2023-08-27 05:18:06 -0400

    zzz

--- src/app/access/access.page.ts
+++ src/app/access/access.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-access',
@@ -7,29 +8,51 @@ import { Component } from '@angular/core';
 })
 export class AccessPage {
 
+  
+  loggedInUser: string = 'Usuario123'; 
   displayForm: boolean = false;
   client = {
     firstName: '',
     lastName: '',
+    educationLevel: '',
     birthDate: null
   };
 
-  constructor() {}
+  constructor(private alertController: AlertController) {}
 
   showClientForm() {
     this.displayForm = !this.displayForm;
   }
 
-  saveClient() {
-    // Aquí puedes implementar la lógica para guardar el cliente.
-    console.log(this.client);
+  async showClientDetails() {
+    const alert = await this.alertController.create({
+      header: 'Detalles del Cliente',
+      message: `${this.client.firstName} ${this.client.lastName}`,
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
+
+  clearClientDetails() {
     this.client = {
       firstName: '',
       lastName: '',
+      educationLevel: '',
+      birthDate: null
+    };
+  }
+
+  saveClient() {
+    console.log(this.client); // Aquí puedes guardar el cliente en una base de datos o servicio si lo deseas.
+    
+    // Después de guardar el cliente, reseteamos los valores y ocultamos el formulario.
+    this.client = {
+      firstName: '',
+      lastName: '',
+      educationLevel: '',
       birthDate: null
     };
     this.displayForm = false;
   }
 
-  // Otros métodos y lógica pueden seguir aquí...
 }
