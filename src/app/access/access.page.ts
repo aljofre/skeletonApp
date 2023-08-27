88e420307bd6bce9278c8ac71c0453eb26d26afd
Alvaro Jofre <a.jofrema@gmail.com>
2023-08-27 09:08:40 -0400

    sss

--- src/app/access/access.page.ts
+++ src/app/access/access.page.ts
@@ -26,7 +26,82 @@ export class AccessPage {
   showClientForm() {
     this.displayForm = !this.displayForm;
   }
-
+  import { Component } from '@angular/core';
+  import { ActionSheetController, AlertController } from '@ionic/angular';
+  
+  @Component({
+    selector: 'app-access',
+    templateUrl: 'access.page.html',
+    styleUrls: ['access.page.scss']
+  })
+  export class AccessPage {
+  
+    loggedInUser: string = 'Usuario123'; 
+    displayForm: boolean = false;
+    animateInput: boolean = false;
+    client = {
+      firstName: '',
+      lastName: '',
+      educationLevel: '',
+      birthDate: null
+    };
+  
+    constructor(
+      private actionSheetController: ActionSheetController,
+      private alertController: AlertController
+    ) {}
+  
+    showClientForm() {
+      this.displayForm = !this.displayForm;
+    }
+  
+    async showClientDetails() {
+      const alert = await this.alertController.create({
+        header: 'Detalles del Cliente',
+        message: `${this.client.firstName} ${this.client.lastName}`,
+        buttons: ['OK']
+      });
+      await alert.present();
+    }
+  
+    clearClientDetails() {
+      this.animateInput = true;
+      setTimeout(() => {
+        this.animateInput = false;
+      }, 1000);
+  
+      this.client = {
+        firstName: '',
+        lastName: '',
+        educationLevel: '',
+        birthDate: null
+      };
+    }
+  
+    saveClient() {
+      console.log(this.client);
+      
+      this.client = {
+        firstName: '',
+        lastName: '',
+        educationLevel: '',
+        birthDate: null
+      };
+      this.displayForm = false;
+      this.showSaveSuccessAlert();
+    }
+  
+    // Método para mostrar el alerta después de guardar.
+    async showSaveSuccessAlert() {
+      const alert = await this.alertController.create({
+        header: '¡Éxito!',
+        message: 'Guardado con éxito.',
+        buttons: ['OK']
+      });
+      await alert.present();
+    }
+  }
+  
   async showClientDetails() {
     const alert = await this.alertController.create({
       header: 'Detalles del Cliente',
